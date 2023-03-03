e170143f603055732b815346ac03d30699df7766
ethanolx <imotekhx@gmail.com>
2023-03-03 21:10:29 +0800

    T1Q1: All tests passed

--- src/1/cuboid.c
+++ src/1/cuboid.c
@@ -1,8 +1,7 @@
-#ifndef CUBOID
-#define CUBOID
+#ifndef CUBOID_C
+#define CUBOID_C
 
 #include "cuboid.h"
-#include "math.h"
 
 double hypotenuse_of(long a, long b, long c) {
   double _a = (double)a;
@@ -13,6 +12,34 @@ double hypotenuse_of(long a, long b, long c) {
 
 long area_of_rectangle(long a, long b, long c) {
   return 2 * a * b + 2 * b * c + 2 * a * c;
+}
+
+bool cuboid_handler(const char in_buffer[][MAX_LINE_LENGTH],
+                    const int num_lines,
+                    const char out_buffer[][MAX_LINE_LENGTH], char message[]) {
+  long a, b, c;
+  sscanf(in_buffer[0], "%ld %ld %ld", &a, &b, &c);
+
+  // Expected
+  double hyp_exp;
+  long area_exp;
+  sscanf(out_buffer[0], "%ld", &area_exp);
+  sscanf(out_buffer[1], "%lf", &hyp_exp);
+
+  // Actual
+  double hyp = round_p(hypotenuse_of(a, b, c), 4);
+  long area = area_of_rectangle(a, b, c);
+
+  if (hyp != hyp_exp) {
+    sprintf(message, "Hypotenuse: Expected %lf but received %lf", hyp_exp, hyp);
+    return false;
+  }
+  if (area != area_exp) {
+    sprintf(message, "Area: Expected %ld but received %ld", area_exp, area);
+    return false;
+  }
+
+  return true;
 }
 
 #endif
--- src/1/cuboid.h
+++ src/1/cuboid.h
@@ -1,4 +1,14 @@
+#ifndef CUBOID_H
+#define CUBOID_H
+
+#include "../utils.h"
 #include <math.h>
+#include <stdio.h>
 
 double hypotenuse_of(long, long, long);
 long area_of_rectangle(long, long, long);
+
+bool cuboid_handler(const char[][MAX_LINE_LENGTH], const int,
+                    const char[][MAX_LINE_LENGTH], char[]);
+
+#endif
